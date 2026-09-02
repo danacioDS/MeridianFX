0cc84c4c2d1a291410d8a6f7a9090c93c660b52c
danacioDS <danacio@gmail.com>
2026-09-02 17:42:24 -0400

    Fix: Corregir importaciones y build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MainLayout } from "./components/layout/MainLayout";
 import { GlobalPage } from "./pages/GlobalPage";
-import { ForecastPage } from "./pages/ForecastPage";
+import ForecastPage from "./pages/ForecastPage";
 import { DriversPage } from "./pages/DriversPage";
 import { EvaluationPage } from "./pages/EvaluationPage";
 import { StatusPage } from "./pages/StatusPage";
